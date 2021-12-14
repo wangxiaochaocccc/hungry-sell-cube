e8257e9152b7cd86d47239b597d4aa669c47f855
wangchao <wangxiaochaocccc@163.com>
2021-12-14 15:19:53 +0800

    feat:mock数据

--- hungry-sell-cube/vue.config.js
+++ hungry-sell-cube/vue.config.js
@@ -1,3 +1,5 @@
+const dataAll = require('./data.json')
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -13,6 +15,28 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function(req, res) {
+        res.json({
+          error: 0,
+          data: dataAll.seller
+        })
+      }),
+      app.get('/api/goods', function(req, res) {
+        res.json({
+          error: 0,
+          data: dataAll.goods
+        })
+      }),
+      app.get('/api/ratings', function(req, res) {
+        res.json({
+          error: 0,
+          data: dataAll.ratings
+        })
+      })
     }
   }
 }
